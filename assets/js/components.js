c71ce18ebe574ba28a066888f44bf1e1c5efd84c
Volodya Sokha <volodya.sokha@gmail.com>
2019-04-05 17:49:35 +0300

    Components Added

---
+++ assets/js/components.js
@@ -0,0 +1,35 @@
+Vue.component('main_section', { 
+	template: '#main-template' 
+})
+
+Vue.component('workspace_section', { 
+	template: '#workspace-template' 
+})
+
+Vue.component('projects_section', { 
+	template: '#projects-template' 
+})
+
+Vue.component('statistics_section', { 
+	template: '#statistics-template' 
+})
+
+Vue.component('team_section', { 
+	template: '#team-template' 
+})
+
+Vue.component('apps_section', { 
+	template: '#apps-template' 
+})
+
+Vue.component('admin_section', { 
+	template: '#admin-template' 
+})
+
+Vue.component('blog_section', { 
+	template: '#blog-template' 
+})
+
+Vue.component('settings_section', { 
+	template: '#settings-template' 
+})
